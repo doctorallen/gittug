8bb9c9f08b8e5cec1dfd6ae72a139e206c80fc47
David Allen <drallen1@plymouth.edu>
2013-05-17 11:56:02 -0400

    working script

--- gittug.pl
+++ gittug.pl
@@ -13,14 +13,19 @@ $file_names=`git diff --name-only HEAD\@{1}`;
 #split the diff into an array for each filename
 @split_names=split('\n', $file_names);
 #loop over each name
-foreach my $name (@split_names) {
+foreach my $file_name (@split_names) {
 	#set the name variable to default for regex
-	$_ = $name;
+	$_ = $file_name;
 	#if the file is an sql file
 	if ( /\.sql$/ ) {
+		#get the name of the procedure, without the extension
+		my $name = substr($file_name, 0, -4);
 		#do magic
-		print "SQL:$name";
+		print "SQL:$file_name";
+		print "NAME:$name";
+		`mysql -utest -ptest test -e 'drop procedure if exists $name;'`;
+		`mysql -utest -ptest test < $file_name`;
 	}
-	print $name;
+	print $file_name;
 
 }
