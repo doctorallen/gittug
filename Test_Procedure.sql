a7aa108b4105f5eba6b25f8d7726ac979eda2d52
David Allen <drallen1@plymouth.edu>
2013-05-17 15:42:16 +0000

    added test procedure

---
+++ Test_Procedure.sql
@@ -0,0 +1,21 @@
+MITER ;;
+
+/*!50003 CREATE*/ /*!50020 DEFINER=`test`@`localhost`*/ /*!50003 PROCEDURE `Test_Procedure`(
+
+p_OperatorID INTEGER,
+
+p_BillUpdatedID INTEGER,
+
+p_Discount FLOAT
+
+)
+
+BEGIN
+
+INSERT INTO PMRT.PMRT_BillingTransactions (DATETIME, OperatorID, TypeID, SubTypeID, BillID, Amount)
+
+    VALUES (NOW(),p_OperatorID, 6, 5,p_BillID,p_Discount);
+
+END */;;
+
+DELIMITER ;
